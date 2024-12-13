4665bc811167a9ed63d4d5d5a238788d289a554e
Giovvni <giovanni.vasquez@alumnos.uv.cl>
2024-12-12 21:38:04 -0300

    se elimina la función comentarios

--- memeologia_front/src/app/page.tsx
+++ memeologia_front/src/app/page.tsx
@@ -1,28 +1,20 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import CommentModal from "./components/CommentModal";
 import { useRouter } from "next/navigation";
 
 interface Meme {
   id: string;
   imageUrl: string;
   likes: number;
-  comments: { id: number; text: string; author: string; authorAvatar: string; likes: number }[];
   reports: number;
   reported: boolean;
 }
 
 const Inicio: React.FC = () => {
   const [memes, setMemes] = useState<Meme[]>([]);
-  const [selectedMemeId, setSelectedMemeId] = useState<string | null>(null);
-  const [newComment, setNewComment] = useState("");
   const [page, setPage] = useState(1);
   const router = useRouter();
   const [showReportModal, setShowReportModal] = useState(false);
-
-  const handleCommentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewComment(event.target.value);
-  };
 
   useEffect(() => {
     const fetchMemes = async () => {
@@ -41,53 +33,6 @@ const Inicio: React.FC = () => {
 
     fetchMemes();
   }, [page]);
-
-  const openComments = (memeId: string) => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      router.push("/login");
-      return;
-    }
-    setSelectedMemeId(memeId);
-  };
-
-  const closeComments = () => {
-    setSelectedMemeId(null);
-  };
-
-  const addComment = async (memeId: string, newComment: string) => {
-    const token = localStorage.getItem("token");
-    const usuarioId = localStorage.getItem("usuarioId");
-
-    try {
-      const response = await fetch(`http://localhost:8000/memes/${memeId}/comments`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          usuario_id: usuarioId,
-          contenido: newComment,
-          meme_id: memeId,
-        }),
-      });
-
-      if (response.ok) {
-        const updatedMeme = await response.json();
-        setMemes((prevMemes) =>
-          prevMemes.map((meme) =>
-            meme.id === memeId ? { ...meme, comments: updatedMeme.comments } : meme
-          )
-        );
-        setNewComment(""); 
-      } else {
-        console.error("Error al agregar el comentario");
-      }
-    } catch (error) {
-      console.error("Error de conexión:", error);
-    }
-  };
 
   const likeMeme = async (memeId: string) => {
     const token = localStorage.getItem("token");
@@ -176,13 +121,6 @@ const Inicio: React.FC = () => {
 
           <div className="p-4 flex justify-between items-center">
             <button
-              onClick={() => openComments(meme.id)}
-              className="focus:outline-none hover:bg-gray-300 px-4 py-2 rounded-lg flex items-center space-x-2"
-            >
-              <img src="/icons/comments.png" alt="Comentario" className="w-6 h-6" />
-              <span className="font-semibold">Comentar</span>
-            </button>
-            <button
               onClick={() => likeMeme(meme.id)}
               className="focus:outline-none hover:bg-gray-300 px-4 py-2 rounded-lg flex items-center space-x-2"
             >
@@ -218,19 +156,6 @@ const Inicio: React.FC = () => {
           Siguiente
         </button>
       </div>
-
-      {selectedMemeId && (
-        <CommentModal
-          isOpen={Boolean(selectedMemeId)}
-          onClose={closeComments}
-          comments={memes.find((meme: Meme) => meme.id === selectedMemeId)?.comments || []}
-          newComment={newComment}
-          onNewCommentChange={handleCommentChange}
-          onAddComment={async () => {
-            await addComment(selectedMemeId!, newComment);
-          }}
-        />
-      )}
 
       {showReportModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
